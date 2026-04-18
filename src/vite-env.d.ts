57801a37cd750aab8dc5aef9f2706aed754a7b29
waqas-subhane <waqassubhani99@gmail.com>
2026-04-18 20:59:45 +0530

    first commit

---
+++ src/vite-env.d.ts
@@ -0,0 +1,14 @@
+/// <reference types="vite/client" />
+
+declare module '*.css';
+declare module '*.scss';
+declare module '*.sass';
+declare module '*.less';
+declare module '*.styl';
+declare module '*.png';
+declare module '*.jpeg';
+declare module '*.jpg';
+declare module '*.svg';
+declare module '*.gif';
+declare module '*.webp';
+declare module '*.avif';
